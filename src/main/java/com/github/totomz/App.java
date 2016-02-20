01e3cc50cba30d392d1c373a3ab58fdd00922eee
tommaso <tommaso.doninelli@my-ideas.it>
2016-02-20 19:12:42 +0100

    Created project structure

---
+++ src/main/java/com/github/totomz/App.java
@@ -0,0 +1,13 @@
+package com.github.totomz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
